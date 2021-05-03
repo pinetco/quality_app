9b359f9a281fdf6a2e51a8bbef6ad3393025eb4d
Nitingadhiya <vthink.nitin@gmail.com>
2021-05-03 19:43:55 +0530

    push notification -done Refactoring - inprogress

--- lib/firebase_notification.dart
+++ lib/firebase_notification.dart
@@ -1,52 +1,35 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:flutter/material.dart';
-
-const AndroidNotificationChannel channel = AndroidNotificationChannel(
-  'high_importance_channel', // id
-  'High Importance Notifications', // title
-  'This channel is used for important notifications.', // description
-  importance: Importance.high,
-);
+import 'package:quality_app/packages/config_package.dart';
 
 class FirebaseNotification {
   String _token;
   Stream<String> _tokenStream;
 
-  Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-    // If you're going to use other Firebase services in the background, such as Firestore,
-    // make sure you call `initializeApp` before using other Firebase services.
-    await Firebase.initializeApp();
-    //print('Handling a background message **** ${message.messageId}');
-  }
-
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-
   initNotification() async {
-    print('init');
-    await Firebase.initializeApp();
+    // print('init');
+    // await Firebase.initializeApp();
+    //
+    // // Set the background messaging handler early on, as a named top-level function
+    // FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+    //
+    // /// Create an Android Notification Channel.
+    // ///
+    // /// We use this channel in the `AndroidManifest.xml` file to override the
+    // /// default FCM channel to enable heads up notifications.
+    // await flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()?.createNotificationChannel(channel);
+    //
+    // /// Update the iOS foreground notification presentation options to allow
+    // /// heads up notifications.
+    // await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    //   alert: true,
+    //   badge: true,
+    //   sound: true,
+    // );
 
-    // Set the background messaging handler early on, as a named top-level function
-    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
-    /// Create an Android Notification Channel.
-    ///
-    /// We use this channel in the `AndroidManifest.xml` file to override the
-    /// default FCM channel to enable heads up notifications.
-    await flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()?.createNotificationChannel(channel);
-
-    /// Update the iOS foreground notification presentation options to allow
-    /// heads up notifications.
-    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-      alert: true,
-      badge: true,
-      sound: true,
-    );
-    Future.delayed(Duration(seconds: 2), () {
-      getToken();
-      listenNotification();
-    });
+    getToken();
+    //listenNotification();
   }
 
   getToken() {
@@ -57,11 +40,12 @@ class FirebaseNotification {
     // _tokenStream = FirebaseMessaging.instance.onTokenRefresh;
     // _tokenStream.listen(setToken);
   }
-
+/*
   listenNotification() {
     FirebaseMessaging.instance.getInitialMessage().then((RemoteMessage message) {
       if (message != null) {
-        //Navigator.pushNamed(context, '/message', arguments: MessageArguments(message, true));
+        Get.toNamed(AppRouter.login);
+//        Navigator.pushNamed(context, '/message', arguments: MessageArguments(message, true));
       }
     });
 
@@ -88,7 +72,8 @@ class FirebaseNotification {
 
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
       print('A new onMessageOpenedApp event was published!');
+      Get.toNamed(AppRouter.login);
       // Navigator.pushNamed(context, '/message', arguments: MessageArguments(message, true));
     });
-  }
+  }*/
 }
